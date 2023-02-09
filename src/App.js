cee8b434c5c9ca91056de45fe812eeb607312866
shahrukh khan <shahrukh.khan072@gmail.com>
2023-02-09 20:37:03 +0530

    add contact details

--- src/App.js
+++ src/App.js
@@ -1,54 +1,84 @@
-import useFetch from 'react-fetch-hook'
-import ContactCards from './ContactCards'
-import {useEffect, useState} from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const url = 'https://randomuser.me/api/'
-  const { data } = useFetch(url+'?results=200')
-  const [contactList, setContactList] = useState()
-  const [filterQuery, setFilterQuery] = useState()
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    if (!filterQuery) {
-      setContactList(data?.results?.slice(0, 10))
-    } else {
-      const queryString = filterQuery.toLowerCase()
-      const filteredData = data?.results?.filter(contact => {
-        const fullName = `${contact.name.first} ${contact.name.last}`
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setContacts(data));
+  }, []);
 
-        // if it's just one letter, return all names that start with it
-        if (queryString.length === 1) {
-          const firstLetter = fullName.charAt(0).toLowerCase()
-          return firstLetter === queryString
-        }
-        else {
-          return fullName.toLowerCase().includes(queryString)
-        }
-      })
-      setContactList(filteredData)
-    }
-  }, [data, filterQuery])
+  const addContact = (contact) => {
+    fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      body: JSON.stringify(contact),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setContacts([...contacts, data]));
+  };
+
+  const updateContact = (contact) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${contact.id}`, {
+      method: "PUT",
+      body: JSON.stringify(contact),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        setContacts(
+          contacts.map((c) => (c.id === data.id ? { ...data } : c))
+        )
+      );
+  };
+
+  const deleteContact = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => setContacts(contacts.filter((c) => c.id !== id)));
+  };
 
   return (
-    <div className={"bg-gray-100"}>
-      <section>
-        <form>
-          <input
-            type={"text"}
-            placeholder={"type here to filter..."}
-            onChange={event => setFilterQuery(event.target.value)}
-            className={"ml-20 mt-6 rounded-md p-2"}
-          />
-        </form>
-      </section>
-      <section className={"grid sm:grid-cols-2 md:grid-cols-4 gap-6 p-20"}>
-        {contactList?.length < 1 && (
-          <h1>No data matches your search</h1>
-        )}
-        <ContactCards contactList={contactList}/>
-      </section>
+    <div>
+      <h1>Contact List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>{contact.id}</td>
+              <td>{contact.name}</td>
+              <td>{contact.email}</td>
+              <td>
+                <button onClick={() => updateContact(contact)}>Update</button>
+                <button onClick={() => deleteContact(contact.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={() => addContact({ name: "New Contact", email: "test@test.com" })}>
+        Add Contact
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
